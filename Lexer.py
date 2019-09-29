74053111ebe0b758ae6552f2b314290d3a0faf40
Abeeaad97 <abidbakhtiyar@csu.fullerton.edu>
2019-09-28 18:27:26 -0700

    Cleaned code and debugged certain issues

--- Lexer.py
+++ Lexer.py
@@ -1,8 +1,14 @@
+"""
+CPSC 232 Compilers Project 1
+Members:
+Abid Bakhtiyar
+Moustapha Said
+"""
 keywords = ["int", "float", "bool", "if", "else", "then", "do", "while", "whileend", "do", "doend", "for", "and", "or",
             "function"]  # lists of key values that will be used
 # to identify the token
-separator = ['\'', '(', ')', '{', '}', '[', ']', ',', '.', ':', ';', '!', ' ']
-operators = ['*', '+', '-', '=', '/', '>', '<', '%', ]
+separator = ['\'', '(', ')', '{', '}', '[', ']', ',', '.', ':', ';', '!']
+operators = ['*', '+', '-', '=', '/', '>', '<', '%']
 
 #             char      num      "!"     sep     "."     op     "\n"
 # new            1        2        4       0       0      0       0
@@ -12,7 +18,8 @@ operators = ['*', '+', '-', '=', '/', '>', '<', '%', ]
 # comment        4        4        0       4       4      4       4
 # invalid         0        0        0       0       0      0       0
 
-statetable = [  # state table that uses coordinates of current states[rows] and passed character state[columns]
+statetable = [
+    # state table that traverses through current states[rows] and passed char states [column]
     [1, 2, 4, 0, 0, 0, 0],
     [1, 1, 4, 0, 0, 0, 0],
     [5, 2, 4, 0, 3, 0, 0],
@@ -23,22 +30,24 @@ statetable = [  # state table that uses coordinates of current states[rows] and 
 ]
 
 
+# A series of tests that checks what the current state is and what the passed character state
+# is to determine what coordinates should be retrieved from the state table
+
 def testChar(char, state):
-    # A series of tests that checks what the current state is and what the passed character state
-    # is to determine what coordinates should be retrieved from the state table
+    # tests what the current state is and what the passed character state is to find what what should be retrieved
+    # from the state table
     currentState = state
-    # print("currentState is: ", currentState)
-    if char.isalpha():  # checks if the character is a alphabetical character
+    if char.isalpha():  # checks for alphabetic characters
         currentState = statetable[currentState][0]
         return currentState
 
-    elif char.isdigit() or (char == "$"):  # checks if the character is a digit or a $
+    elif char.isdigit() or (char == "$"):  # checks for a digit or a $
         currentState = statetable[currentState][1]
         return currentState
-    elif char == "!":  # checks if the character is a "!"
+    elif char == "!":  # checks check for "!"
         currentState = statetable[currentState][2]
         return currentState
-    elif char == ".":  # checks if the character is a "."
+    elif char == ".":  # checks for "."
         currentState = statetable[currentState][4]
         return currentState
     elif char.isspace():  # checks if the character is a " "
@@ -46,23 +55,19 @@ def testChar(char, state):
         return currentState
     else:
         if (char in operators) or (
-                char.isspace()):  # checks if the character is in the operator list or a space
+                char.isspace()):  # searches if character is an operator or its a space
             currentState = statetable[currentState][5]
             return currentState
         elif (char in separator) or (
-                char.isspace()):  # checks if the character is in the separator list or a space
+                char.isspace()):  # checks if character is a separator or its a space
             currentState = statetable[currentState][3]
             return currentState
 
 
-def showTemp(temp, output):  # This is where we will check what type of token is our temporary parsed word
-    if temp != "":  # If our temp is blank, then ignore everything and consider as a space and do nothing
-        # is_keyword = False  # bool phrase that will keep track if the word is a keyword or an identifier
-        is_float = False  # bool phrase that will keep track if the word is an integer or a float
-        # print("temp here is: ", temp)
-        if temp in keywords:  # if our temp word is inside the list of keywords, then write into the output file that
-            # it is a keyword
-            #   print("Keyword")
+def showTemp(temp, output):  # Decides if token is an integer or float
+    if temp != "":  # If our temp is blank, consider it a space
+        is_float = False  # checks if it's a float or integer
+        if temp in keywords:  # if our temp word is inside the list of keywords, then write into the output file
             output.write(temp + "    =     keyword\n")
             return
         is_keyword = True
@@ -73,24 +78,23 @@ def showTemp(temp, output):  # This is where we will check what type of token is
             output.write(temp + "    =     identifier\n")
             return
 
-        for a in temp:  # checking to see of the temp word has a period which would indicate that it is no longer a
-            # digit
+        for a in temp:  # checks if temp word has a "." meaning it has to be a float
             if a == ".":
                 is_float = True
 
-        if not is_float:  # if our temp word has no decimal, that it cannot be a float but a digit
+        if not is_float:  # if not a float it has to be an integer
             output.write(temp + "    =     Integer\n")
             return
 
-        elif is_float:  # if our temp word has a decimal, than it cannot be digit but a float
+        elif is_float:  # if it has decimal it is a float
             output.write(temp + "    =     Float\n")
             return
 
-        output.write(temp + "    =     Invalid\n")  # if all tests above failed, then the word is an invalid word
+        output.write(temp + "    =     Invalid\n")  # base case
         return
 
 
-def showChar(char, output):  # Checks if our single character is a operator or a separator
+def showChar(char, output):  # decides with char is operator or separator
     temp = char
     if char in operators:  # checks if our character is in the list of operators
         output.write(temp + "    =     Operator\n")
@@ -99,41 +103,32 @@ def showChar(char, output):  # Checks if our single character is a operator or a
 
 
 def main():
-    with open('SampleInputFile2.txt',
-              'r') as file:  # opens a text file and saves its data as file. MUST CHANGE TEXT FILE IF TESTING WITH
+    with open('SampleInputFile.txt', 'r') as file:
+        # opens a text file and saves its data as file. MUST CHANGE TEXT FILE IF TESTING WITH
         # DIFFERENT FILE TYPE
 
         temp = " "
-        # char = " "
         currentstate = 0
 
-        output = open("output.txt", "w+")  # creates an output file that will store the token and lexeme
+        output = open("output.txt", "w+")  # creates an output file
         data = file.read()
-        sentencesbank = data.splitlines()  # parses data into a list of sentences
-        for sentences in sentencesbank:  # parses list of sentences into single sentences
-            #   print(sentences)
-            for a in range(len(sentences)):  # parses single sentence into characters
-                #      print("a is: ", a)
+        sentencesBank = data.splitlines()  # parses data into a list of sentences
+        for sentences in sentencesBank:  # puts list of sentences into a single sentence
+            for a in range(len(sentences)):  # Parses the sentence into individual chars
                 char = sentences[a]
-                #     print("char is now: ", char)
-                #    print("currentstate is: ", currentstate)
-                if currentstate != 4:  # checks if our current state isn't a comment block
+                if currentstate != 4:  # make sure the current state is not a comment
                     currentstate = testChar(char, currentstate)
-                    #       print("new current state is: ", currentstate)
                     if currentstate == 0:
                         showTemp(temp, output)
                         showChar(char, output)
                         temp = ""
 
-                    elif currentstate == 1 or currentstate == 2 or currentstate == 3:  # our current states allows us
-                        # to add the character into our temporary word
+                    elif currentstate == 1 or currentstate == 2 or currentstate == 3:
+                        # our current states allows us to add a character to our word
                         temp += char
-                    #          print("in state 1 2 3 and temp is: ", temp)
 
                     elif currentstate == 4:  # our current state checks what temp is
                         showTemp(temp, output)
-                        #         print("tmep is: ", temp)
-                        #        print("here in state 4")
                         temp = ""
 
                     elif currentstate == 5:  # checks if we get an invalid word
@@ -143,10 +138,8 @@ def main():
 
                 elif (char == "!") and (currentstate == 4):
                     currentstate = statetable[currentstate][2]
-                #       print("current state is now :", currentstate)
 
     showTemp(temp, output)
-    # temp = " "
     output.close()  # closes output file
     print("lexeme               token")
     print("---------------------------")
